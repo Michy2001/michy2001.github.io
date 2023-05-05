f122f9f79da16ddf5a78c4b49aa4ddbe3ca01c1c
Michelle <yangm730@newschool.edu>
2023-05-05 09:15:29 -0400

    vs code

--- nycdata.js
+++ nycdata.js
@@ -1,16 +1,16 @@
 if (/AppleWebKit\/([\d.]+)/.exec(navigator.userAgent)) {
-    var e = null;
-    document.addEventListener(
-      "click",
-      function () {
-        var t = window.event.target;
-        e &&
-          "true" == e.contentEditable &&
-          "true" !== t.contentEditable &&
-          (document.getElementById("editableFix").setSelectionRange(0, 0),
-          t.focus()),
-          (e = t);
-      },
-      !1
-    );
-  }
+  var e = null;
+  document.addEventListener(
+    "click",
+    function () {
+      var t = window.event.target;
+      e &&
+        "true" == e.contentEditable &&
+        "true" !== t.contentEditable &&
+        (document.getElementById("editableFix").setSelectionRange(0, 0),
+        t.focus()),
+        (e = t);
+    },
+    !1
+  );
+}
